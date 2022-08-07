33ba33d842959c7d7958ba910fab09d5f0604c86
maisiesadler <maisiesadler@hotmail.com>
2022-08-07 11:34:04 +0100

    src

---
+++ src/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace TestToDocs;
+
+public static class WebApplicationFactoryExtensions
+{
+    // public static HttpClient CreateRecordedClient(this WebApplicationFactory<T> webApplicationFactory)
+    // {
+    //     return webApplicationFactory.CreateDefaultClient(_recordingHandler);
+    // }
+}
